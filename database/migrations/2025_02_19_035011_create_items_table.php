e95fe174be2480628a208431ecdc1a4897ccde1e
Pangeran <pangeransilaen1417@gmail.com>
2025-02-20 08:01:04 +0700

    progress

---
+++ database/migrations/2025_02_19_035011_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('stock');
+
+            $table->foreignId('jetski_id')->constrained();
+            $table->foreignId('pilihpaket_id')->nullable()->constrained();
+
+            $table->text('photos')->nullable();
+
+            $table->integer('review')->default(0);
+
+            $table->softDeletes();
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
